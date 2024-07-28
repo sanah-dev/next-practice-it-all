822593e3380f86a8af34e430f39def9be932c79e
박산아 <dev.sanah@gmail.com>
2024-07-29 05:59:41 +0900

    에러로 배포가 안되서 잠시 제거

--- app/list/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import commonStyles from '../../../styles/common.module.css';
-
-export default function Loading() {
-  return (
-    <div className={commonStyles.container}>
-      <h2 className={commonStyles.loading}>Loading a book...</h2>
-    </div>
-  );
-}
